627678d392e656ce2da8d741a8da70af6764906a
sanburnn <dimas00758@gmail.com>
2021-09-16 18:22:53 +0700

    gabut

--- lib/kategori.dart
+++ lib/kategori.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:users/theme.dart';
 
+class Kategori extends StatelessWidget {
+  const Kategori({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
